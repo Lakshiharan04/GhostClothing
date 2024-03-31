26c1a8ade0700cb05158fcfbc7f91337c7139ec3
Lakshiharan04 <cobsccomp4y222p-045@student.nibm.lk>
2024-03-31 16:43:08 +0530

    Created database and intergrated he api into the application

---
+++ GhostClothing/Model/ProductModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProductModel.swift
+//  GhostClothing
+//
+//  Created by NIBM on 2024-03-31.
+//
+
+import Foundation
---
+++ GhostClothing/ViewModel/ProductViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProductViewModel.swift
+//  GhostClothing
+//
+//  Created by NIBM on 2024-03-31.
+//
+
+import Foundation
